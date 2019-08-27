f45ca8707f304dcac80b7d7d8fa86e398a239553
Chirag Ahuja <chiragahuja.nsit@gmail.com>
2019-08-27 14:54:20 +0530

    checking support for commit hash

--- travis/buildAndDeploy.sh
+++ travis/buildAndDeploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-docker build -f Dockerfile -t foo .
+docker build -f Dockerfile --build-arg COMMITHASH=$TRAVIS_REPO_SLUG -t bar .
 
+# Deploying to heroku
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
+heroku container:relase web --app $HEROKU_APP_NAME
 
