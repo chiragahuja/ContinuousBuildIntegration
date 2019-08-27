735dd5d54f817684c66641547fc29b5421375043
Chirag Ahuja <chiragahuja.nsit@gmail.com>
2019-08-27 16:08:25 +0530

    added commit hash support to docker in heroku

--- travis/buildAndDeploy.sh
+++ travis/buildAndDeploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -x
 docker build --build-arg COMMITHASH=$TRAVIS_COMMIT -t bar .
+
 # Deploying to heroku
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
+heroku container:push web --app $HEROKU_APP_NAME --arg COMMITHASH=$TRAVIS_COMMIT
 heroku container:release web --app $HEROKU_APP_NAME
 
-printenv
 
