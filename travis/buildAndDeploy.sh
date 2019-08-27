cb81d4b7cfb390a99c40b82ff4358aee05d77653
Chirag Ahuja <chiragahuja.nsit@gmail.com>
2019-08-27 15:37:34 +0530

    printint travis envs

--- travis/buildAndDeploy.sh
+++ travis/buildAndDeploy.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-docker build -f Dockerfile --build-arg COMMITHASH=$TRAVIS_REPO_SLUG -t bar .
-
+docker build --build-arg COMMITHASH=$TRAVIS_REPO_SLUG -t bar .
 # Deploying to heroku
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
+
+printenv
 
